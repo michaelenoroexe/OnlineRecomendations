7bfa8d5ff34e981adf7b072ddce040bff5bfeac2
Michaelenoroexe <serezha.alexandrovitch2018@yandex.ru>
2023-11-06 11:52:19 +0300

    Added PI to ci and configured database for development.

---
+++ bin/postgress-setup.cmd
@@ -0,0 +1,1 @@
+docker run -e "POSTGRES_PASSWORD=TestPassword" -p 5432:5432 -d --name PostgressDB postgres:16.0-alpine3.18
---
+++ bin/postgress-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -e "POSTGRES_PASSWORD=TestPassword" -p 5432:5432 -d --name PostgressDB postgres:16.0-alpine3.18
